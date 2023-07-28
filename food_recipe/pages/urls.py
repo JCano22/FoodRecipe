75c5cb144b5043275eff821d8c4e2ea921273f97
Jorge Cano <coronero@gmail.com>
2023-07-28 16:18:22 -0700

    Capstone
    Added views for pages and recipes Apps, connected to Edamam API, created recipe model for search results, ran migrations, created superuser

--- food_recipe/pages/urls.py
+++ food_recipe/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, search_recipes
 
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
+    path("results/", search_recipes, name='results')
 
 ]
--- food_recipe/pages/views.py
+++ food_recipe/pages/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from recipes.views import fetch_and_save_recipe
 
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+
+# view function to search api with keyword from user
+
+
+def search_recipes(request):
+    if request.method == 'POST':
+        search_query = request.POST.get('search_query', '')
+
+        search_results = fetch_and_save_recipe(search_query)
+
+        return render(request, 'pages/results.html', {'results': search_results})
+    return render(request, 'home.html')
