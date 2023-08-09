50f9eee16dd0f7027a430605d3567c4f39d73679
Eloim2023 <124946573+Eloim2023@users.noreply.github.com>
2023-08-08 18:09:39 -0700

    5th commit

--- food_recipe/pages/urls.py
+++ food_recipe/pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import HomePageView, search_recipes, recipe_detail
+from . import views
 
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
     path("results/", search_recipes, name='results'),
     path("results/<int:recipe_id>/", recipe_detail, name='details'),
-
+    path("account_info/", views.account_info_view, name='account_info'),
+    #path('edit-account/', views.edit_account, name='edit_account'),
 ]
--- food_recipe/pages/views.py
+++ food_recipe/pages/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 from recipes.views import fetch_and_save_recipe
 from recipes.models import Recipe
+from django.contrib.auth.decorators import login_required
+#from .forms import EditAccountForm
 
 
 class HomePageView(TemplateView):
@@ -29,3 +31,24 @@ def recipe_detail(request, recipe_id):
         'recipe': recipe
     }
     return render(request, 'pages/detail.html', context)
+
+@login_required
+def account_info_view(request):
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'pages/account_info.html', context)
+
+#def edit_account(request):
+    user = request.user
+
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('account_info')  # Redirect to the account info page after saving changes
+    else:
+        form = EditAccountForm(instance=user)
+
+    return render(request, 'pages/edit_account.html', {'form': form})
