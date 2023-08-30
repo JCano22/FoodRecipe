81fad71aa2c7061b951c4247e92d56b0118c8833
Eloim2023 <124946573+Eloim2023@users.noreply.github.com>
2023-08-29 18:00:39 -0700

    8th commit

--- food_recipe/accounts/urls.py
+++ food_recipe/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import SignUpView
+from . import views 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
+    path("account_info/", views.account_info_view, name='account_info'),
+    path('edit_account/', views.edit_account, name='edit_account'),
+
 ]
--- food_recipe/pages/urls.py
+++ food_recipe/pages/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
     path("results/", search_recipes, name='results'),
     path("results/<int:recipe_id>/", recipe_detail, name='details'),
-    path("account_info/", views.account_info_view, name='account_info'),
-    #path('edit-account/', views.edit_account, name='edit_account'),
 ]
