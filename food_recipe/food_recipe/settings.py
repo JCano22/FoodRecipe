f1cd2d4b06c35bbea18a09ee1b3c354c80faa482
Eloim2023 <124946573+Eloim2023@users.noreply.github.com>
2023-09-03 10:28:23 -0700

    adding db

--- food_recipe/food_recipe/settings.py
+++ food_recipe/food_recipe/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com", ".azurewebsites.net", "fsdi-foodrecipe.azurewebsites.net"]
 
 
 # Application definition
